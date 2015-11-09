d8f321916c37f5af3ba3ede079c283525521421f
Luís Ferreira <lsferreira169@gmail.com>
2015-11-09 21:58:40 +0000

    c portugal translation update :+1:

--- C/functions/portugal.h
+++ C/functions/portugal.h
@@ -5,23 +5,23 @@
 //Include Libraries
 #include <stdio.h>
 
-    void imprimir(char* strputs;
-    void caracter(char charputchar);
-    void caracterentrada(char chargetchar);
-    void entrada(char* strgets);
-    int quociente(int divindendo, int divisor);
-    int resto(int divindendo, int divisor);
-    int quocientelongo(int divindendo, int divisor);
-    int restolongo(int divindendo, int divisor);
-	int fraseparainteiro(char* stratoi);
-	long int fraseparainteirolongo(char* stratol);
-	double fraseparadecimaldouble(char* stratof);
-	float fraseparadecimalfloat(char* stratof);
-	void remover(char* strremove);
-	void renomear(char* strold, char* strnew);
-	void entradacaracter(char charscanf);
-	void entradainteiro(int intscanf);
-	void entradadecimal(float floatscanf);
-	void entradafrase(char* strscanf);
+    void imprimir(char* strputs); //puts()
+    void caracter(char charputchar); //putchar()
+    void caracterentrada(char chargetchar); //getchar()
+    void entrada(char* strgets); //gets()
+    int quociente(int divindendo, int divisor); //div()
+    int resto(int divindendo, int divisor); // "
+    int quocientelongo(int divindendo, int divisor); //ldiv()
+    int restolongo(int divindendo, int divisor); // "
+	int fraseparainteiro(char* stratoi); //atoi()
+	long int fraseparainteirolongo(char* stratol); //atol()
+	double fraseparadecimaldouble(char* stratof); //atof()
+	float fraseparadecimalfloat(char* stratof); // "
+	void remover(char* strremove); // remove()
+	void renomear(char* strold, char* strnew); // rename()
+	void entradacaracter(char charscanf); // scanf()
+	void entradainteiro(int intscanf); // "
+	void entradadecimal(float floatscanf); // "
+	void entradafrase(char* strscanf); // "
 
 #endif // PORTUGAL_H_INCLUDED
