8fd7089546f99b118b6ba6f2d0e2cff72aeb9e3e
Luís Ferreira <lsferreira169@gmail.com>
2015-11-15 19:55:17 +0000

    php update

---
+++ PHP/index.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+	<link rel="icon" href="/res/favicon.ico" type="image/x-icon"/>
+	<link rel="shortcut icon" href="/res/favicon.ico" type="image/x-icon"/>
+	<link rel="shortcut icon" href="/favicon.ico?v=2">
+		<!-- Custom styles for this template -->
+	<link rel="stylesheet" type="text/css" href="res/style.css"> 
+	<link rel="stylesheet" type="text/css" href="res/main.css"> 
+		<!-- Scripts -->
+	<script src="res/jquery.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="/bootstrap/js/bootstrap.min.js"></script>
+	<script src="res/smoothscroll.js"></script>
+		<!-- Bootstrap -->
+	<link href="/bootstrap/css/bootstrap.css" rel="stylesheet">
+	<link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<form class="form-horizontal">
+<fieldset>
+
+<!-- Button (Double) -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="button1id">Select:</label>
+  <div class="col-md-8">
+    <button id="btwhois" class="btn btn-info" formaction="/whois.php">Whois</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
